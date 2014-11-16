15a437aa2e80e58b1c5bbb8a36a65154dd48f06c
Masha Westerlund <masha.westerlund@gmail.com>
2014-11-15 23:24:54 -0500

    added regex filtering of tweets

--- findExpat.py
+++ findExpat.py
@@ -2,6 +2,7 @@ import tweepy
 import os
 from firebase import *
 import json
+import re
 
 
 auth = tweepy.OAuthHandler(os.environ['consumer_token'], os.environ['consumer_secret'])
@@ -13,18 +14,21 @@ class StdOutListener(tweepy.streaming.StreamListener):
 
     def on_data(self, data):
         decoded = json.loads(data)
-        if decoded['geo'] and decoded['lang'] == 'en' and decoded['place']['country'] == 'United States':
+        if decoded['geo'] and decoded['lang'] and decoded['place']: #and decoded['place']['country'] == 'United States':
 
             
             x, y = decoded['geo']['coordinates']
-            tweet = decoded['text']
-            # print coords
-            # print decoded['place']['country']
-           
+            tweet = decoded['text']  
+            # print tweet     
 
-            
-            self.store_data((x,y,tweet))
-            # print json.dumps(decoded, indent = 4, separators=(',', ': '))
+            if decoded['lang'] == 'en' and decoded['place']['country'] == 'United States': 
+
+                # print json.dumps(decoded, indent = 4, separators=(',', ': '))
+                
+                print tweet
+
+                if self.isCanadian(tweet):
+                    self.store_data((x,y,tweet))
 
 
     def store_data(self, coords):
@@ -34,6 +38,18 @@ class StdOutListener(tweepy.streaming.StreamListener):
 
     def on_error(self, error):
         print error
+
+    def isCanadian(self, tweet):
+
+        # c= re.compile("t")
+        c = re.compile("(\\beh\?|,eh\?)", re.I)
+        canadian = c.search(tweet)
+        if canadian:
+            return True
+        else: return False
+        #might do something something fancier here later to handle complicated 'eh' situations
+
+
 
 if __name__ == '__main__':
     l = StdOutListener()
